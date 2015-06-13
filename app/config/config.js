11b179b99c0022cb761636d5478f01a2f2092e3e
davenich <davenich@gmail.com>
2015-06-13 15:58:58 -0400

    housekeeping

--- app/config/config.js
+++ app/config/config.js
@@ -2,14 +2,9 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp.config', ['firebase.utils'])
-
-  // version of this seed app is compatible with angularFire 1.0.0
-  // see tags for other versions: https://github.com/firebase/angularFire-seed/tags
+  
   .constant('version', '0.0.1')
 
-  // where to redirect users if they need to authenticate (see security.js)
-  .constant('loginRedirectPath', '/login')
-
-  // Todo: need to move this to another file
-  // your Firebase data URL goes here, no trailing slash
+  .constant('loginRedirectPath', '/signin')
+    
   .constant('FBURL', 'https://09sdfgs.firebaseio.com/');
