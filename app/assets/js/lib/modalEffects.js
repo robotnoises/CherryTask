b1eecc51b7826a7792e7fc96d80178d36044034c
David Nichols <davenich@gmail.com>
2015-06-23 15:18:03 -0400

    overlay no longer kills modal

--- app/assets/js/lib/modalEffects.js
+++ app/assets/js/lib/modalEffects.js
@@ -35,8 +35,6 @@ var ModalEffects = (function() {
 
 			el.addEventListener( 'click', function( ev ) {
 				classie.add( modal, 'md-show' );
-				overlay.removeEventListener( 'click', removeModalHandler );
-				overlay.addEventListener( 'click', removeModalHandler );
 
 				if( classie.has( el, 'md-setperspective' ) ) {
 					setTimeout( function() {
