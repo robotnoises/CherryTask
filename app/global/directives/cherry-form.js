c2bdc143b7f183aff1410c5847617559429b8a79
David Nichols <davenich@gmail.com>
2015-06-24 16:38:28 -0400

    Submit stuff

--- app/global/directives/cherry-form.js
+++ app/global/directives/cherry-form.js
@@ -8,8 +8,8 @@
     return {
       restrict: 'E',
       templateUrl: 'templates/directives/cherryForm.html',
-      controller: ['$scope', '$location', '$timeout', '$q', 'apiService',
-      function cherryFormController ($scope, $location, $timeout, $q, api) {
+      controller: ['$scope', '$location', '$timeout', '$q', '$routeParams', 'apiService',
+      function cherryFormController ($scope, $location, $timeout, $q, $routeParams, api) {
         
         var template = Object.freeze({
           NEW_PROJECT: '',
@@ -21,14 +21,14 @@
           if (mod == template.NEW_PROJECT) {
             $deferred.resolve({ key: 'new_project', apiLoc: 'projects/' });
           } else if (mod == template.NEW_TASK ) {
-            $deferred.resolve({ key: 'new_task', apiLoc: '' });
+            $deferred.resolve({ key: 'new_task', apiLoc: 'tasks/', projectId: $routeParams.id });
           } else {
             $deferred.reject();
           }
           return $deferred.promise;
         };
         
-        var setFormType = function () {
+        var init = function () {
           var uri = $location.$$path.split('/').filter(function(s){ return s !== ''; })[0];
           var $promise = getFormType(uri || '');
           
@@ -40,13 +40,7 @@
           });
         };
         
-        $scope.form = {};
-        
-        $scope.$on('$routeChangeSuccess', function (e) {
-          setFormType();
-        });
-        
-        $scope.matchesFormType = function (formTypeData) {
+        var matchesFormType = function (formTypeData) {
           if (Array.isArray(formTypeData)) {
             for (var i = 0, len = formTypeData.length; i < len; i++) {
               if (formTypeData[i] == $scope.formType) {
@@ -59,18 +53,56 @@
           }
         };
         
+        var reset = function (timeout) {
+                    
+          $scope.cherryForm.$setPristine();
+          $scope.cherryForm.$setUntouched();
+          
+          // Form inputs TODO: there's gotta be a better way than this..
+          $scope.cherryForm.title.$touched = false;
+          $scope.cherryForm.description.$touched = false;
+          
+          $timeout(function () {            
+            toggleSubmitOverlay();
+            $scope.form = {};
+            // Todo: bind a click to submit?
+            angular.element('.modal-close').click();
+          }, timeout || 0);
+        };
+        
+        var toggleSubmitOverlay = function () {
+          var $overlay = angular.element('.submit-overlay');
+          if ($overlay.hasClass('show')) {
+            $overlay.removeClass('show');
+          } else {
+            $overlay.addClass('show');
+          }
+        };
+        
+        $scope.form = {};
+        
+        $scope.$on('$routeChangeSuccess', function (e) {
+          init();
+        });
+        
+        $scope.matchesFormType = matchesFormType;
+        
         $scope.submit = function () {
           api.create($scope.formData.apiLoc, $scope.form, function (snapshot) {
             var obj = angular.copy(snapshot.val());
             obj.id = snapshot.key();
+            if (matchesFormType('new_task')) {
+              obj.projectId = $scope.formData.projectId;
+            }
             api.update($scope.formData.apiLoc + snapshot.key(), obj, obj.id);
+            toggleSubmitOverlay();
+            reset(1000);
           });
         };
         
-        setFormType();
+        init();
         
       }]
-      
     };
   });
 
--- app/modules/project/controllers.js
+++ app/modules/project/controllers.js
@@ -20,28 +20,6 @@
         $scope.projects = projects;
       });
 
-      var _addProject = function () {
-        var temp = {
-          title: 'Random Title ' + Math.floor(Math.random() * 1000) + 1,
-          description: 'Random description ' + Math.floor(Math.random() * 100000000) + 1,
-          dateCreated: new Date().getTime()
-        };
-
-        $scope.projects.$add(temp).then(function update (project) {
-          var key = project.key();
-          $timeout(function () {
-            try {
-              var idx = $scope.projects.$indexFor(key);
-              $scope.projects[idx].id = key;
-              $scope.projects.$save(idx);  
-            } catch (ex) {
-              if (idx < 0) update(project);
-            }
-          },1);
-        });
-      };
-      
-      $scope.addProject = _addProject;
     }])
 
   .controller('projectController', ['$scope', '$routeParams', '$timeout', 'apiService',
@@ -76,20 +54,20 @@
       };
 
       // Scope methods
-      $scope.addTask = function () {
-        $scope.tasks.$add($scope.task).then(function update (task) {
-          var key = task.key();
-          $timeout(function () {
-            try {
-              var idx = $scope.tasks.$indexFor(key);
-              $scope.tasks[idx].id = key;
-              $scope.tasks.$save(idx);  
-            } catch (ex) {
-              if (idx < 0) update(task);
-            }
-          },1);
-        });
-      };
+      // $scope.addTask = function () {
+      //   $scope.tasks.$add($scope.task).then(function update (task) {
+      //     var key = task.key();
+      //     $timeout(function () {
+      //       try {
+      //         var idx = $scope.tasks.$indexFor(key);
+      //         $scope.tasks[idx].id = key;
+      //         $scope.tasks.$save(idx);  
+      //       } catch (ex) {
+      //         if (idx < 0) update(task);
+      //       }
+      //     },1);
+      //   });
+      // };
     }]);
 
 })(angular);
