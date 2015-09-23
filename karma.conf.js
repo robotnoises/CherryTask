c2082ce9af3b4e7e33a306b033440b4cfae79fd5
nicholsd <davenich@gmail.com>
2015-09-23 16:34:43 -0400

    Add angularFileUpload dependency to karma

--- karma.conf.js
+++ karma.conf.js
@@ -17,6 +17,8 @@ module.exports = function (config) {
       'app/bower_components/angularfire/dist/angularfire.js',
       'app/bower_components/firebase/firebase.js',
       'app/bower_components/angular-perfect-scrollbar/src/angular-perfect-scrollbar.js',
+      'app/bower_compenents/angular-file-upload/dist/angular-file-upload.min.js',
+      
       // Test lib
       'tests/lib/*.js',
       // App
