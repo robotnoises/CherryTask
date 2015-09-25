e3e6557926c2c4089f28fd88a6307b2cd9e3b3c7
David Nichols <davenich@gmail.com>
2015-09-25 14:56:52 -0400

    Added a simple card menu (ui-only)

--- app/global/directives/cherrycard.js
+++ app/global/directives/cherrycard.js
@@ -15,8 +15,14 @@
       controller: ['$scope', '$location', '$timeout', '$q', '$routeParams', 'apiService',
       function cherryCardController ($scope, $location, $timeout, $q, $routeParams, api) {
         
+        $scope.menuShowing = false;
+        
         $scope.show = function () {
           $location.path($scope.card.type + 's/' + $scope.card.id);
+        };
+        
+        $scope.toggleShowMenu = function (force) {
+          $scope.menuShowing = force || !$scope.menuShowing;
         };
                       
       }]
